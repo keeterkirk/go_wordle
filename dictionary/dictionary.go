67b1a9a6a9022b579209abd52bf97ea111ce1a16
Kirk Keeter <kirk.keeter@prizepicks.com>
2025-06-19 18:57:50 -0400

    temporarily adds random word supply

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,5 +1,21 @@
 package dictionary
 
+import "math/rand/v2"
+
 func RandomWord() string {
-	return "award"
+
+	temp := getWords()
+	arrayCount := len(temp)
+	picker := rand.IntN(arrayCount)
+	return temp[picker]
+}
+
+func getWords() []string {
+	return []string{
+		"award",
+		"apple",
+		"grape",
+		"peach",
+		"berry",
+	}
 }
